579e6e756ca0b44ca7dae6f07f0997185e8191bf
Akash Singh <akashsingh27101998@gmail.com>
2022-07-16 23:40:39 +0530

    Stack & Queue

--- 14]. Day-14 (Stack and Queue)/InterviewBit/1]. Next Smaller Element.cpp
+++ 14]. Day-14 (Stack and Queue)/InterviewBit/1]. Next Smaller Element.cpp
@@ -1,19 +1,18 @@
-#include<stack>
-
-vector<int> nextSmallerElement(vector<int> &arr, int n)
-{
-    // first we create a stack 
+vector<int> Solution::prevSmaller(vector<int> &A) {
+    int n = A.size();
+    reverse(A.begin(), A.end());
     stack <int> stackk;
     stackk.push(-1);
     vector <int> answer(n);
     for(int i=n-1 ; i>=0 ; i--)
     {
-        int value_of_array = arr[i];
+        int value_of_array = A[i];
         while(stackk.top()>=value_of_array){
             stackk.pop();
         }
         answer[i] = stackk.top();
         stackk.push(value_of_array);
     }
+    reverse(answer.begin(), answer.end());
     return answer ;
 }
